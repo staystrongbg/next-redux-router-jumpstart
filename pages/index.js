d01617341171d7f01f2681f4fd451b7374107b29
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-07-02 12:06:40 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Layout from '../components/Layout';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function Home() {
   return (
