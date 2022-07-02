68c122c8cec4efaf19d96e5c705f9ff7c8c6e9de
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-07-02 12:43:51 +0200

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,13 @@ import Navbar from './Navbar';
 import styles from '@/styles/Main.module.css';
 import Head from 'next/head';
 
-const Layout = ({ children, title }) => {
+const Layout = ({ children, title, keywords, description }) => {
   return (
     <>
       <Head>
         <title>{title}</title>
+        <meta name='keywords' content={keywords} />
+        <meta name='description' content={description} />
       </Head>
       <div className={styles.wrapper}>
         <Navbar />
@@ -19,3 +21,7 @@ const Layout = ({ children, title }) => {
 };
 
 export default Layout;
+Layout.defaultProps = {
+  keywords: 'keywords are accepted as props',
+  description: 'description is accepted as props',
+};
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  env: {},
+  /*
+  ***to use next/Image in SSG 
+  images: {
+    disableStaticImages: false,
+    loader: 'custom',
+  *** refresh dynamic pages
+  trailingSlash: true,
+  },*/
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
